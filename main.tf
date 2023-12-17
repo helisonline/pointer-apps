e04e8bbbbb28dac4bad0426b62631a07f83716d8
EC2 Default User <ec2-user@ip-172-31-16-102.ec2.internal>
2023-12-17 22:34:07 +0000

    rollback key_name2

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ module "ec2_instance" {
 
   ami                    = "ami-058bd2d568351da34"
   instance_type          = "t2.micro"
-  key_name               = "vockey"
+  key_name               = ""
   monitoring             = true
   vpc_security_group_ids = [module.pointer_sg.security_group_id]
   subnet_id              = var.subnet_id
